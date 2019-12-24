f439d665be5f43c7b27733f237cf0d0981475106
王宇楠 <1273652804@qq.com>
2019-12-25 00:50:48 +0800

    django study 3

--- django_HelloWorld/django_HelloWorld/settings.py
+++ django_HelloWorld/django_HelloWorld/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'django_HelloWorld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_helloworld',
+        'HOST':'',
+        'PASSWORD': '123456',
+        'USER': 'root',
     }
 }
 

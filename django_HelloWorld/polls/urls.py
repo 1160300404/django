110ef7092309947347cc51c9ec5c6ddf597572b6
王宇楠 <1273652804@qq.com>
2019-12-28 16:48:41 +0800

    study Django4

--- django_HelloWorld/polls/urls.py
+++ django_HelloWorld/polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello),
+    path('hello/', views.hello),
+    path('index/',views.index,name='index'),
 ]
--- django_HelloWorld/polls/views.py
+++ django_HelloWorld/polls/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import Question
 def hello(request):
     return HttpResponse("hello world i'm studying django")
+def index(request):
+    question_list=Question.objects.all()
+    count=len(question_list)
+    context = {'question_list':question_list,'count':count}
+    return render(request,'polls/index.html',context)
